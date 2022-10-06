08714552324540909edefee46284e940d2cc4c42
Aaron Mak <im@arnmk.com>
2022-10-06 17:01:52 +0800

    Add directory to text search

--- text/replace-string-all-files.sh
+++ text/replace-string-all-files.sh
@@ -4,20 +4,22 @@ if [ -z "$*" ];
   then printf "
 Old String [-r]
 New String [-s]
+Directory to search [-d]
   ";
   exit 1;
 fi
 
-while getopts r:s: option
+while getopts r:s:d: option
 do
  case "${option}"
  in
  r) OLD_STRING=${OPTARG};;
  s) NEW_STRING=${OPTARG};;
+ d) DIR=${OPTARG};;
  *) exit 1;;
  esac
 done
 
 echo  "s/${OLD_STRING}/${NEW_STRING}/"
 
-rg "${OLD_STRING}" -l | xargs sed -i '' "s/${OLD_STRING}/${NEW_STRING}/"
+rg "${OLD_STRING}" "${DIR}" -l | xargs sed -i '' "s/${OLD_STRING}/${NEW_STRING}/"
